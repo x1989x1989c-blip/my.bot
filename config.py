9a712088262ce3833e6439426a8b204df8a65e28
x1989x1989c-blip <x1989x1989c@gmail.com>
2026-09-02 03:05:44 +0000

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 BOT_TOKEN = os.getenv("BOT_TOKEN", "8929555984:AAEVLnYzg6wVmFrpuxICgoLg7t0ttFJcdTg")
 SUPER_ADMIN_ID = int(os.getenv("ADMIN_ID", "8903157513"))
 PORT = int(os.getenv("PORT", 8080))
-WEBAPP_URL = os.getenv("WEBAPP_URL", "https://your-render-app-name.onrender.com")
+WEBAPP_URL = os.getenv("WEBAPP_URL", "https://my-bot-j48l.onrender.com")
 
 DB_FILE = "bot_database.db"
 
